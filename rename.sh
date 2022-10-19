cdba271cb9235be3e1354d12fd9d9332ed081654
KichiroNakamoto <santiago.ruizm40@gmail.com>
2022-10-18 23:25:58 -0500

    011

--- rename.sh
+++ rename.sh
@@ -1,2 +1,3 @@
 "codigo en bash que sirve para nombrar muchos archivos al mismo tiempo"
+"ojo con no poner el $, si no se pierden los documentos"
  n=1; for i in *.png; do mv "$i" "RedConsciousness_$((n++)).png"; done 
