2e0770af9f6b1be2bd4f35d627fb864821692d9d
KichiroNakamoto <santiago.ruizm40@gmail.com>
2022-10-20 23:24:11 -0500

    011

---
+++ javastest.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+class Main {
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("Hello, World!");
+		System.out.println("Hello, Again!");
+		
+		int x = 123; 
+		double x1 = 3.14565;
+		boolean x2 = false;
+		String name = "This is a name";
+		
+		// This is a comment
+		
+		System.out.println("Whats your age?: ");
+		int name_a = scanner.nextline();
+		
+		System.out.println("Whats your wife's age?: ");
+		int name_b = scanner.nextline();
+		
+		
+		System.out.println("The multiplication between both ages, is: ");
+		
+		
+	}
+}
