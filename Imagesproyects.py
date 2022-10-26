8dc80c5279b17b30d13d8745a295e9ea0a83118a
KichiroNakamoto <santiago.ruizm40@gmail.com>
2022-10-26 14:55:16 -0500

    011

---
+++ Imagesproyects.py
@@ -0,0 +1,38 @@
+"""
+1* A generatriz of Artificial inteligence images from standar difussion of Dream Studio
+    1.0.1* Get the Lexica Api to extract the prompts
+    1.0.2* Using the invers ingeneer image search of Lexica to find matchs between the best valorte images in Pinterest
+    1.0.3* Using NPL make a list o mixing prompts NPL_promtp.txt
+    1.0.4* prompts have to be less than 255 characters
+    1.1* At least 3 google collab counts to be active developing images 
+    1.2* Automtization of fake imail to Sing Up in Dream Studio
+    1.2.1* Enter two prompts for fake count, doing 10 searchs and 90 images for each prompt 
+    1.2.2* Categorized and unzip those images in local host. 
+    1.2.3* Put a name with NPL. 
+    1.3* Analize and erase the blur images.
+    1.4* Rename the images with bash.  
+
+
+2* A sizer to better quality and resolution with 3000px and 300dpi with AI and photoshop automatization
+    2.1* Using another 3 google accounts to do the AI resize
+    2.2* Using photoshop automatization to fix dpi's  
+
+3* An upload automatization for instagram and other media
+    3.1* for each 100 publication create and instagram acount
+    3.2* Using and automatization search for each NPL name generated in 1.2.3, create an history. 
+    3.3* do a copy mix of the relevant hashtags in the community respect to the 1.2.3 NPL name. 
+    3.4* Upload the photos with worst reolution and watermark.
+    3.5* Create and automate video reels with Ai Transformations of the images and an inteligent Design. 
+    3.5* Uploads instagram reels. 
+
+4* Arts with more reception to the public media get selected to upload automatically to the NFT and digital art pages
+    4.1* count the most popular publications in the instagram by likes and intereactions
+    4.2* If more than n interactions it goes to NFT if not go to digital store. 
+    4.2.1* Create and account in a page of digital art with a register of count and password.
+    4.2.2* Upload the images in 3000px and 300dpi resolution. With same description and hastags as in 3.3
+    4.2.3* Upload the NFT images to advertising using instagram. 
+    4.2.4* Upload to the Opean Seas and other NFT market places with same descriptions as in 3.3 
+    4.2* Asociate every count with the Respective Paypal and the Wallet account.  
+
+
+"""
